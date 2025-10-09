012dc2f633b7b10281796b96f03da5a63e2b4687
JLin056 <jlin890516@gmail.com>
2025-10-09 14:57:31 +0800

    add login

--- src/main/java/com/example/petel/controller/AuthController.java
+++ src/main/java/com/example/petel/controller/AuthController.java
@@ -1,13 +1,13 @@
 package com.example.petel.controller;
 
 import com.example.petel.controller.advice.BaseController;
-import com.example.petel.dto.AUTH001Tranrq;
-import com.example.petel.dto.AUTH001Tranrs;
-import com.example.petel.dto.Req;
-import com.example.petel.dto.Res;
+import com.example.petel.dto.*;
 import com.example.petel.exception.InsertFailException;
 import com.example.petel.exception.InvalidInputException;
+import com.example.petel.exception.JwtProcessingException;
 import com.example.petel.service.AUTH001Svc;
+import com.example.petel.service.AUTH002Svc;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.Errors;
@@ -20,11 +20,39 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController extends BaseController{
     /** AUTH001 Service */
     private final AUTH001Svc auth001Svc;
+    /** AUTH002 Service */
+    private final AUTH002Svc auth002Svc;
 
+    /**
+     * 註冊
+     * @param req
+     * @param errors
+     * @return
+     * @throws InsertFailException
+     * @throws InvalidInputException
+     */
     @PostMapping("/register")
     public Res<AUTH001Tranrs> register(@Valid @RequestBody Req<AUTH001Tranrq> req, Errors errors)
             throws InsertFailException, InvalidInputException {
         handleValidForDto(errors);
         return auth001Svc.register(req);
+    }
+
+    /**
+     * 登入
+     * @param req
+     * @param resp
+     * @param errors
+     * @return
+     * @throws InvalidInputException
+     * @throws JwtProcessingException
+     */
+    @PostMapping("/login")
+    public Res<AUTH002Tranrs> login(@Valid @RequestBody Req<AUTH002Tranrq> req,
+                                    HttpServletResponse resp,
+                                    Errors errors)
+            throws InvalidInputException, JwtProcessingException {
+        handleValidForDto(errors);
+        return auth002Svc.login(req, resp);
     }
 }

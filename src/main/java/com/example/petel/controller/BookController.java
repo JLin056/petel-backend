46ad5d2832e42afa9f7f06319faf252734104b65
Gationhe <kayonwang@gmail.com>
2025-10-15 14:14:48 +0800

    ready For PR

--- src/main/java/com/example/petel/controller/BookController.java
+++ src/main/java/com/example/petel/controller/BookController.java
@@ -1,13 +1,13 @@
 package com.example.petel.controller;
 
 import com.example.petel.controller.advice.BaseController;
-import com.example.petel.dto.BOOK001Tranrq;
-import com.example.petel.dto.BOOK001Tranrs;
-import com.example.petel.dto.Req;
-import com.example.petel.dto.Res;
+import com.example.petel.dto.*;
+import com.example.petel.exception.DataNotFoundException;
 import com.example.petel.exception.InsertFailException;
 import com.example.petel.exception.InvalidInputException;
+import com.example.petel.exception.UpdateFailException;
 import com.example.petel.service.BOOK001Svc;
+import com.example.petel.service.BOOK003Svc;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.Errors;
@@ -22,9 +22,18 @@ public class BookController extends BaseController {
     /** BOOK001Svc */
     private final BOOK001Svc book001Svc;
 
+    /** BOOK003Svc */
+    private final BOOK003Svc book003Svc;
+
     @PostMapping(value = "/create")
     public Res<BOOK001Tranrs> book001(@Valid @RequestBody Req<BOOK001Tranrq> requestBody, Errors errors) throws InsertFailException, InvalidInputException {
         handleValidForDto(errors);
         return book001Svc.book001(requestBody);
+    }
+
+    @PostMapping(value = "/update")
+    public Res<Object> book003(@Valid @RequestBody Req<BOOK003Tranrq> requestBody, Errors errors) throws DataNotFoundException, UpdateFailException, InvalidInputException {
+        handleValidForDto(errors);
+        return book003Svc.book003(requestBody);
     }
 }

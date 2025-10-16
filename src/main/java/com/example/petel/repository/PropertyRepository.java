81404abb6302d32c6f097572e7a2b9ad5297d4f8
JLin056 <jlin890516@gmail.com>
2025-10-16 22:42:54 +0800

    debug

--- src/main/java/com/example/petel/repository/PropertyRepository.java
+++ src/main/java/com/example/petel/repository/PropertyRepository.java
@@ -5,12 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PropertyRepository extends JpaRepository<PropertyEntity, Long> {
-    /**
-     * 根據賣家 ID 查詢所有相關物業
-     * 
-     * @param sellerId 賣家 ID
-     * @return 物業列表
-     */
-    List<PropertyEntity> findBySellerId(String sellerId);
+public interface PropertyRepository extends JpaRepository<PropertyEntity, String> {
 }

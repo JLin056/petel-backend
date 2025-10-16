b36a8cea14b22024c1ec268e8dbb07c57b4cfc50
Gationhe <kayonwang@gmail.com>
2025-10-16 21:25:46 +0800

    test done

--- src/main/java/com/example/petel/entity/TransactionsEntity.java
+++ src/main/java/com/example/petel/entity/TransactionsEntity.java
@@ -59,7 +59,7 @@ public class TransactionsEntity {
     /**
      * Idempotency Key
      */
-    @Column(name = "IDEMPOTENCY_KEY", nullable = false)
+    @Column(name = "IDEMPOTENCY_KEY")
     private String idempotencyKey;
 
     /**
@@ -71,7 +71,7 @@ public class TransactionsEntity {
     /**
      * 交易手續費
      */
-    @Column(name = "TXN_FEE", nullable = false)
+    @Column(name = "TXN_FEE")
     private Integer transactionFee;
 
     /**
--- src/main/java/com/example/petel/service/impl/BOOK009SvcImpl.java
+++ src/main/java/com/example/petel/service/impl/BOOK009SvcImpl.java
@@ -2,16 +2,17 @@ package com.example.petel.service.impl;
 
 import com.example.petel.entity.TransactionsEntity;
 import com.example.petel.exception.PaymentFailedException;
+import com.example.petel.model.IdUtil;
 import com.example.petel.model.TimeUtil;
 import com.example.petel.model.book.CodeUtil;
 import com.example.petel.repository.TransactionsRepository;
 import com.example.petel.service.BOOK009Svc;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -19,7 +20,7 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
- * BOOK-011 綠界通知付款回應 (線上刷卡) SvcImpl
+ * BOOK-009 綠界通知付款回應 (線上刷卡) SvcImpl
  */
 @Slf4j
 @Service
@@ -38,34 +39,48 @@ public class BOOK009SvcImpl implements BOOK009Svc {
     /** HASH_IV */
     @Value("${ecpay.hashIv}")
     private String HASH_IV;
+    /** PAYING_LOCK */
+    private static final Object PAYING_LOCK = new Object();
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public String book009(Map<String, Object> requestParam) throws Exception {
 
-        TransactionsEntity transactionsEntity = new TransactionsEntity();
-        transactionsEntity.setOrderId(requestParam.get("MerchantTradeNo").toString().replaceFirst("^PETEL", ""));
-        transactionsEntity.setPaymentId(PAYMENT_ID);
-        transactionsEntity.setTransactionId(requestParam.get("TradeNo").toString());
-        transactionsEntity.setFlowType("Pay");
-        transactionsEntity.setHotelCharges((Integer) requestParam.get("TradeAmt"));
-        transactionsEntity.setCreatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+        synchronized (PAYING_LOCK) {
 
-        if ((Integer) requestParam.get("RtnCode") != SUCCESS_RTN_CODE) {
-            transactionsEntity.setStatus("付款失敗");
-            log.error("[BOOK-009] 付款失敗");
-            throw new PaymentFailedException();
+            TransactionsEntity transactionsEntity = new TransactionsEntity();
+            transactionsEntity.setId(IdUtil.generateTableId("T", transactionsRepository.findMaxId()));
+            transactionsEntity.setOrderId(requestParam.get("MerchantTradeNo").toString().replaceFirst("^PETEL", ""));
+            transactionsEntity.setPaymentId(PAYMENT_ID);
+            transactionsEntity.setTransactionId(requestParam.get("TradeNo").toString());
+            transactionsEntity.setFlowType("Pay");
+            transactionsEntity.setHotelCharges(Integer.parseInt(requestParam.get("TradeAmt").toString()));
+            transactionsEntity.setCreatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+
+            if (Integer.parseInt(requestParam.get("RtnCode").toString()) != SUCCESS_RTN_CODE) {
+                transactionsEntity.setStatus("付款失敗");
+                transactionsRepository.save(transactionsEntity);
+                log.error("[BOOK-009] 付款失敗");
+                throw new PaymentFailedException();
+            }
+
+            if (!CodeUtil.generateCheckMacValue(requestParam, HASH_KEY, HASH_IV).equals((requestParam.get("CheckMacValue").toString()))) {
+                transactionsEntity.setStatus("付款異常");
+                transactionsRepository.save(transactionsEntity);
+                log.error("[BOOK-009] 檢查碼相異");
+                throw new PaymentFailedException();
+            }
+
+            transactionsEntity.setStatus("付款成功");
+            transactionsEntity.setIdempotencyKey(UUID.randomUUID().toString());
+            if (requestParam.get("PaymentTypeChargeFee") == null) {
+                transactionsEntity.setTransactionFee(0);
+            } else {
+                transactionsEntity.setTransactionFee(Integer.parseInt(requestParam.get("PaymentTypeChargeFee").toString()));
+            }
+            transactionsEntity.setPayTime(TimeUtil.parseMerchantTradeDate(requestParam.get("TradeDate").toString()));
+            transactionsRepository.save(transactionsEntity);
         }
-
-        if (!CodeUtil.generateCheckMacValue(requestParam, HASH_KEY, HASH_IV).equals(requestParam.get("CheckMacValue").toString())) {
-            transactionsEntity.setStatus("付款異常");
-            log.error("[BOOK-009] 檢查碼相異");
-            throw new PaymentFailedException();
-        }
-
-        transactionsEntity.setStatus("付款成功");
-        transactionsEntity.setIdempotencyKey(UUID.randomUUID().toString());
-        transactionsEntity.setTransactionFee((Integer) requestParam.get("PaymentTypeChargeFee"));
-        transactionsEntity.setPayTime(TimeUtil.parseMerchantTradeDate(requestParam.get("MerchantTradeDate").toString()));
 
         return "1|OK";
     }

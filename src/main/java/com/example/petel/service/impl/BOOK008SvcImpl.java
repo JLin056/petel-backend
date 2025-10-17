6aa138789519952f7dc3691923e1624e22d1f85a
Gationhe <kayonwang@gmail.com>
2025-10-17 09:35:06 +0800

    add the communication of DB PETEL_ORDERS

--- src/main/java/com/example/petel/service/impl/BOOK008SvcImpl.java
+++ src/main/java/com/example/petel/service/impl/BOOK008SvcImpl.java
@@ -1,11 +1,13 @@
 package com.example.petel.service.impl;
 
 import com.example.petel.dto.BOOK008Tranrq;
+import com.example.petel.entity.OrdersEntity;
 import com.example.petel.entity.TransactionsEntity;
 import com.example.petel.exception.PaymentFailedException;
 import com.example.petel.model.IdUtil;
 import com.example.petel.model.TimeUtil;
 import com.example.petel.model.book.CodeUtil;
+import com.example.petel.repository.OrdersRepository;
 import com.example.petel.repository.TransactionsRepository;
 import com.example.petel.service.BOOK008Svc;
 import jakarta.transaction.Transactional;
@@ -30,6 +32,8 @@ public class BOOK008SvcImpl implements BOOK008Svc {
 
     /** TransactionsRepository */
     private final TransactionsRepository transactionsRepository;
+    /** OrdersRepository */
+    private final OrdersRepository ordersRepository;
     /** PAYMENT_ID */
     private static final String PAYMENT_ID = "Y000000001";
     /** SUCCESS_RTN_CODE */
@@ -53,17 +57,27 @@ public class BOOK008SvcImpl implements BOOK008Svc {
             JSONObject orderInfoJsonObject = jsonObject.getJSONObject("OrderInfo");
 
             TransactionsEntity transactionsEntity = new TransactionsEntity();
-            transactionsEntity.setId(IdUtil.generateTableId("T", transactionsRepository.findMaxId()));
-            transactionsEntity.setOrderId(orderInfoJsonObject.getString("MerchantTradeNo").replaceFirst("^PETEL", ""));
-            transactionsEntity.setPaymentId(PAYMENT_ID);
-            transactionsEntity.setTransactionId(orderInfoJsonObject.getString("TradeNo"));
-            transactionsEntity.setFlowType("Pay");
-            transactionsEntity.setHotelCharges(orderInfoJsonObject.getInt("TradeAmt"));
-            transactionsEntity.setCreatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+            String orderId = orderInfoJsonObject.getString("MerchantTradeNo").replaceFirst("^PETEL", "");
+
+            if (transactionsRepository.findByOrderId(orderId).isPresent()) {
+                transactionsEntity = transactionsRepository.findByOrderId(orderId).get();
+            } else {
+                transactionsEntity.setId(IdUtil.generateTableId("T", transactionsRepository.findMaxId()));
+                transactionsEntity.setOrderId(orderId);
+                transactionsEntity.setPaymentId(PAYMENT_ID);
+                transactionsEntity.setTransactionId(orderInfoJsonObject.getString("TradeNo"));
+                transactionsEntity.setFlowType("Pay");
+                transactionsEntity.setHotelCharges(orderInfoJsonObject.getInt("TradeAmt"));
+                transactionsEntity.setCreatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+            }
 
             if ((jsonObject.getInt("RtnCode") != SUCCESS_RTN_CODE) || (!"1".equals(orderInfoJsonObject.getString("TradeStatus")))) {
                 transactionsEntity.setStatus("付款失敗");
                 transactionsRepository.save(transactionsEntity);
+                OrdersEntity ordersEntity = ordersRepository.findById(orderId).get();
+                ordersEntity.setStatus("未付款");
+                ordersEntity.setUpdatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+                ordersRepository.save(ordersEntity);
                 log.warn("[BOOK-008] 付款失敗");
                 return "1|OK";
             }
@@ -77,6 +91,10 @@ public class BOOK008SvcImpl implements BOOK008Svc {
             }
             transactionsEntity.setPayTime(TimeUtil.parseMerchantTradeDate(orderInfoJsonObject.getString("TradeDate")));
             transactionsRepository.save(transactionsEntity);
+            OrdersEntity ordersEntity = ordersRepository.findById(orderId).get();
+            ordersEntity.setStatus("已付款");
+            ordersEntity.setUpdatedAt(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
+            ordersRepository.save(ordersEntity);
         }
         log.info("[BOOK-008] 付款成功");
         return "1|OK";

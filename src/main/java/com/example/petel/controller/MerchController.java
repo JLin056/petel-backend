f40cbeb8eeb3958ee752a1152cb15eede9dd8517
00550399 <linnnnng910410@gmail.com>
2025-10-17 10:24:22 +0900

    merge dev

--- src/main/java/com/example/petel/controller/MerchController.java
+++ src/main/java/com/example/petel/controller/MerchController.java
@@ -1,17 +1,26 @@
 package com.example.petel.controller;
 
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.petel.controller.advice.BaseController;
 import com.example.petel.dto.MERCH004Tranrq;
 import com.example.petel.dto.MERCH004Tranrs;
+import com.example.petel.dto.MERCH009Tranrq;
+import com.example.petel.dto.MERCH009Tranrs;
 import com.example.petel.dto.Req;
 import com.example.petel.dto.Res;
 import com.example.petel.exception.InsertFailException;
 import com.example.petel.exception.InvalidInputException;
 import com.example.petel.service.MERCH004Svc;
+import com.example.petel.service.MERCH009Svc;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,39 +33,17 @@ public class MerchController extends BaseController {
      */
     private final MERCH004Svc merch004Svc;
 
+    /**
+     * MERCH009 Service
+     */
+    private final MERCH009Svc merch009Svc;
+
     @PostMapping(value = "/rooms/create")
     public Res<MERCH004Tranrs> create(@Valid @RequestBody Req<MERCH004Tranrq> merch004Tranrq, Errors errors)
             throws InsertFailException, InvalidInputException {
         handleValidForDto(errors);
         return merch004Svc.create(merch004Tranrq);
     }
-}
-package com.example.petel.controller;
-
-import com.example.petel.controller.advice.BaseController;
-import com.example.petel.dto.MERCH009Tranrq;
-import com.example.petel.dto.MERCH009Tranrs;
-import com.example.petel.dto.Req;
-import com.example.petel.dto.Res;
-import com.example.petel.exception.*;
-import com.example.petel.service.MERCH009Svc;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/merchants")
-@CrossOrigin("http://localhost:4200")
-public class MerchController extends BaseController {
-
-    /**
-     * MERCH009 Service
-     */
-    private final MERCH009Svc merch009Svc;
 
     @PostMapping(value = "/sellers/create")
     public Res<MERCH009Tranrs> createSeller(@Valid @RequestBody Req<MERCH009Tranrq> merch009Tranrq, Errors errors)

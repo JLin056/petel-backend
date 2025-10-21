153e924d0bae970ee8f7dcaaa358bd9540f41c2f
00550399 <linnnnng910410@gmail.com>
2025-10-21 15:21:44 +0900

    修改notblank部分

--- src/main/java/com/example/petel/dto/MERCH007Tranrq.java
+++ src/main/java/com/example/petel/dto/MERCH007Tranrq.java
@@ -24,62 +24,53 @@ public class MERCH007Tranrq implements Serializable {
      * name
      */
     @JsonProperty("name")
-    @NotBlank(message = "name不得為空")
     private String name;
 
     /**
      * tel
      */
     @JsonProperty("tel")
-    @NotBlank(message = "tel不得為空")
     private String tel;
 
     /**
      * postal code
      */
     @JsonProperty("postalCode")
-    @NotBlank(message = "postalCode不得為空")
     private String postalCode;
 
     /**
      * address
      */
     @JsonProperty("address")
-    @NotBlank(message = "address不得為空")
     private String address;
 
     /**
      * bank account
      */
     @JsonProperty("bankAccount")
-    @NotBlank(message = "bankAccount不得為空")
     private String bankAccount;
 
     /**
      * info
      */
     @JsonProperty("info")
-    @NotBlank(message = "info不得為空")
     private String info;
 
     /**
      * check notice
      */
     @JsonProperty("checkNotice")
-    @NotBlank(message = "checkNotice不得為空")
     private String checkNotice;
 
     /**
      * pet notice
      */
     @JsonProperty("petNotice")
-    @NotBlank(message = "petNotice不得為空")
     private String petNotice;
 
     /**
      * property notice
      */
     @JsonProperty("propertyNotice")
-    @NotBlank(message = "propertyNotice不得為空")
     private String propertyNotice;
 }

e003a9ed10e4911b02ea7b055c0a0a64b87220b6
Wayne-Huang123 <a0952274142@gmail.com>
2025-10-25 17:46:00 +0800

    修改sql語法(加括號)

--- src/main/resources/sql/Admin007_Count.sql
+++ src/main/resources/sql/Admin007_Count.sql
@@ -4,7 +4,7 @@ FROM
 LEFT JOIN
     PETEL_ACCOUNTS a ON u.ACCOUNT_ID = a.ID
 WHERE
-    a.ROLE = 'USER' or a.ROLE = 'user'
+    (a.ROLE = 'USER' or a.ROLE = 'user')
     [AND a.ID LIKE :accountId]
     [AND a.EMAIL LIKE :email]
     [AND u.NAME LIKE :name]

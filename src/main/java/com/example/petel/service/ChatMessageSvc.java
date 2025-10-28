d8f830a5fe98839b5a7929df91f6294629c97b7e
JLin056 <jlin890516@gmail.com>
2025-10-29 00:21:30 +0800

    refactor WebSocket

--- src/main/java/com/example/petel/service/ChatMessageSvc.java
+++ src/main/java/com/example/petel/service/ChatMessageSvc.java
@@ -7,5 +7,8 @@ import com.example.petel.model.jwt.AccountPrincipal;
 
 public interface ChatMessageSvc {
 
-    void handleIncomingMessage(String threadId, ChatMessage msg, AccountPrincipal sender) throws JwtProcessingException, InvalidInputException;
+    void handleIncomingMessage(String threadId, ChatMessage msg, AccountPrincipal sender)
+            throws JwtProcessingException, InvalidInputException;
+
+    void handleReadEvent(String threadId, String lastMessageId, AccountPrincipal reader) throws JwtProcessingException, InvalidInputException;
 }
